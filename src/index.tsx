4613531d8f734a6c88e1505bd9a9a1512cb8ca58
Myungjin Kim <weumj1@gmail.com>
2018-06-18 15:03:16 +0900

    init

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+function Hello() {
+    return <div>Hello </div>;
+}
+
+ReactDOM.render(<Hello />, document.getElementById("app"));
